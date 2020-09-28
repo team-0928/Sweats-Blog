e6addc488edf6ff16bec27e12c6fa22cd538ebf8
mimizudansyaku <sknm2525@docomo.ne.jp>
2020-09-28 20:33:22 +0900

    initial commit

---
+++ db/migrate/20200919122121_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.integer :price
+      t.string :store
+      t.string :address
+      t.string :image
+      t.text :comment
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end

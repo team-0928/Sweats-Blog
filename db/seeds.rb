2c7619044c01b3d3a9fdbb5486841c9adec625a4
mimizudansyaku <sknm2525@docomo.ne.jp>
2020-10-02 00:08:00 +0900

    delete seed post

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,9 +24,9 @@ User.create!(name:  name,
 end
 
 # ユーザーの一部を対象にマイクロポストを生成する
-users = User.order(:created_at).take(6)
-50.times do
-  name = Faker::Name.name
-  content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.posts.create!(name: name, content: content) }
-end
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   name = Faker::Name.name
+#   content = Faker::Lorem.sentence(word_count: 5)
+#   users.each { |user| user.posts.create!(name: name, content: content) }
+# end

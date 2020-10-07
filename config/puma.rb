56d7c78827ab14ec3b544db6531f362d73f75fb8
mimizudansyaku <sknm2525@icloud.com>
2020-10-07 01:11:16 +0000

    mod puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,8 @@ bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+#environment ENV.fetch("RAILS_ENV") { "development" }
+environment 'production'
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/puma.pid" }

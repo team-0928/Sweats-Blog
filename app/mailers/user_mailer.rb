0f53763eab8f9fcc2e11be4c7f765b49fb038b58
mimizudansyaku <sknm2525@docomo.ne.jp>
2020-10-11 16:19:20 +0900

    update SendGrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,24 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.account_activation.subject
   #
   def account_activation(user)
-    @user = user
+    # using SendGrid's Ruby Library
 
-    mail to: user.email, subject: "アカウント有効化のお願い"
+    # using SendGrid's Ruby Library
+    # https://github.com/sendgrid/sendgrid-ruby
+    require 'sendgrid-ruby'
+    include SendGrid
+
+    from = Email.new(email: '	appstest108@gmail.com')
+    to = Email.new(email: user.email)
+    subject = 'Sending with SendGrid is Fun'
+    content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+    mail = Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml

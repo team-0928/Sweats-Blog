aacd15acd88fedf52bbc0255b59ee9d0e0e6f8e6
mimizudansyaku <sknm2525@icloud.com>
2020-10-13 03:46:55 +0000

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sweets-blog.herokuapp.com/'
+  host = 'www.sweets-blog.awssample92.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -74,7 +74,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'awssample92.com',
     :enable_starttls_auto => true
   }
 

7cb0f4888032d3c67f963510c8f9f34b46ca3eef
mimizudansyaku <sknm2525@docomo.ne.jp>
2021-01-11 12:42:08 +0000

    modified sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'www.sweets-blog.awssample92.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',

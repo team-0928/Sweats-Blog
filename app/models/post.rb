7aa9f2507028b9f34380038025fa3308283b0867
mimizudansyaku <sknm2525@docomo.ne.jp>
2021-02-08 16:30:18 +0900

    modified search function

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,7 +23,7 @@ class Post < ApplicationRecord
 
   def self.search(search)
     return Post.all unless search
-    Post.where(['content LIKE ?', "%#{search}%"])
+    Post.where(['(name LIKE ?) OR (store LIKE ?) OR (address LIKE ?) OR (content LIKE ?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
   end
   # def user
   #   User.find(self.user_id)
